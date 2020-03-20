3d8ca44eb1cefd319960ded3ecb5efef1d45ce28
jnousiainen <joonas.nousiainen@eficode.com>
2020-03-20 16:29:49 +0200

    Static shit works.

--- routes/ws/chat.go
+++ routes/ws/chat.go
@@ -1,57 +1,60 @@
-package ws;
+package ws
 
 import (
-    "net/http"
-    "fmt"
-    "strings"
-    "container/list"
+	"container/list"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"joonas.ninja-chat/util"
 
 	"github.com/gorilla/websocket"
 )
 
 type chatRequest struct {
-	Type string;
-	Message string;
+	Type    string
+	Message string
 }
 
-var clients = list.New();
+var users = list.New()
 
 var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
 func reader(connection *websocket.Conn) {
-    for {
-        messageType, message, err := connection.ReadMessage();
-        if (err != nil) {
-            fmt.Println(err);
-            return
-        }
-        fmt.Println(string(message));
+	for {
+		messageType, message, err := connection.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(message))
 
-        if err := connection.WriteMessage(messageType, message); err != nil {
-            fmt.Println(err);
-            return
-        }
-    }
+		if err := connection.WriteMessage(messageType, message); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
 
 func newChatConnection(connection *websocket.Conn) {
-    fmt.Println("chatRequest(): Connection opened.");
-    reader(connection);
+	fmt.Println("chatRequest(): Connection opened.")
+	users.PushBack(util.User{Name: "kikkare", Connection: connection});
+	reader(connection)
 }
 
 // WebsocketRequest - Handles websocket requests and conveys them to the handler depending on request path.
 func WebsocketRequest(responseWriter http.ResponseWriter, request *http.Request) {
-    upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-    wsConnection, err := upgrader.Upgrade(responseWriter, request, nil);
-    if (err != nil) {
-        fmt.Println(err);
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	wsConnection, err := upgrader.Upgrade(responseWriter, request, nil)
+	if err != nil {
+		fmt.Println(err)
 	}
-    pathArray := strings.Split(request.RequestURI, "/");
+	pathArray := strings.Split(request.RequestURI, "/")
 
-    if (pathArray[len(pathArray) - 1] == "chat") {
-        newChatConnection(wsConnection);
-    }
+	if pathArray[len(pathArray)-1] == "chat" {
+		newChatConnection(wsConnection)
+	}
 }
