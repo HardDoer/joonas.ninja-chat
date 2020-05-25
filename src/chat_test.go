6ac3778240459e5a2cfc5e927634a9dd0b6303dc
JNokikana <herrapentti@gmail.com>
2020-05-26 00:26:30 +0300

    More tests and better structure.

--- src/chat_test.go
+++ src/chat_test.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -13,37 +12,75 @@ import (
 	"testing"
 )
 
+// TODO. Return something more useful.
 func chatHistoryTest(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "{}")
 }
 
-func setupChatHistory() {
-	http.HandleFunc("/test/chatHistory", chatHistoryTest)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Panic(err)
-	}
-}
-
-func TestMessageJoinAndNormalMessage(t *testing.T) {
-	var responseData EventData
-	os.Setenv("CHAT_HISTORY_URL", "http://localhost:8080/test/chatHistory")
-	go setupChatHistory()
+func testSetup(t *testing.T) (*websocket.Conn, *httptest.Server) {
 	server := httptest.NewServer(http.HandlerFunc(ChatRequest))
-	defer server.Close()
 	url := "ws" + strings.TrimPrefix(server.URL, "http")
 	ws, _, err := websocket.DefaultDialer.Dial(url, nil)
 	assert.Nil(t, err)
+	return ws, server
+}
+
+func setupChatHistory() *httptest.Server {
+	chatHistoryServer := httptest.NewServer(http.HandlerFunc(chatHistoryTest))
+	os.Setenv("CHAT_HISTORY_URL", chatHistoryServer.URL)
+	return chatHistoryServer
+}
+
+func TestJoin(t *testing.T) {
+	var responseData EventData
+	ws, server := testSetup(t)
+	defer func() {
+		server.Close()
+		ws.Close()
+	}()
 	_, message, err := ws.ReadMessage()
+	assert.Nil(t, err)
 	readerError := json.Unmarshal(message, &responseData)
 	assert.Nil(t, readerError)
 	assert.Equal(t, true, strings.HasPrefix(responseData.Body, "Anon") && responseData.Body != "Anon", "Name should be of the form AnonSomething")
+}
+
+func TestSendMessage(t *testing.T) {
+	var responseData EventData
+	ws, server := testSetup(t)
+	chathistoryServer := setupChatHistory()
+	defer func() {
+		server.Close()
+		ws.Close()
+		chathistoryServer.Close()
+	}()
+	_, _, err := ws.ReadMessage()
 	assert.Nil(t, err)
 	testRequest := EventData{Event: EventMessage, Body: "Testing message"}
 	jsonResponse, err := json.Marshal(testRequest)
 	assert.Nil(t, err)
 	assert.Nil(t, ws.WriteMessage(websocket.TextMessage, jsonResponse))
-	_, message, err = ws.ReadMessage()
-	readerError = json.Unmarshal(message, &responseData)
+	_, message, err := ws.ReadMessage()
+	readerError := json.Unmarshal(message, &responseData)
 	assert.Nil(t, readerError)
 	assert.Equal(t, true, responseData.Body == "Testing message" && responseData.UserCount == 1, responseData.Event == EventMessage)
+}
+
+func TestChangeName(t *testing.T) {
+	var responseData EventData
+	ws, server := testSetup(t)
+	defer func() {
+		server.Close()
+		ws.Close()
+	}()
+	_, _, err := ws.ReadMessage()
+	assert.Nil(t, err)
+	testRequest := EventData{Event: EventNameChange, Body: "TestDude"}
+	jsonResponse, err := json.Marshal(testRequest)
+	assert.Nil(t, err)
+	assert.Nil(t, ws.WriteMessage(websocket.TextMessage, jsonResponse))
+	_, message, err := ws.ReadMessage()
+	readerError := json.Unmarshal(message, &responseData)
+	assert.Nil(t, readerError)
+	assert.Equal(t, true, responseData.Body == "TestDude" && responseData.UserCount == 1, responseData.Event == EventNameChange)
 }
