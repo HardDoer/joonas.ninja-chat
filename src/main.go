ac0bb40b79bb4b5b87c7285c14ded91becdb59bf
Joonas Nousiainen <herrapentti@gmail.com>
2020-06-02 11:41:39 +0300

    Fixed.

--- src/main.go
+++ src/main.go
@@ -13,18 +13,18 @@ func initEnvFile() {
 	if err != nil {
 		log.Panic("Error loading app.env file. Please create one next to me.")
 	}
-	log.Println("initEnvFile(): Loaded envs.")
+	log.Print("initEnvFile():", "Loaded envs.")
 }
 
 func initRoutes() {
 	http.HandleFunc("/api/v1/ws/chat", ChatRequest)
-	log.Println("initRoutes(): Routes initialized.")
+	log.Print("initRoutes():", "Routes initialized.")
 }
 
 func main() {
 	initEnvFile()
 	initRoutes()
-	log.Println("main(): Starting server...")
+	log.Print("main():", "Starting server...")
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		log.Panic(err)
 	}
